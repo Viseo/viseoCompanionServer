0def277405083c50222600f1b4a262f09c236b88
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-04-20 15:00:08 +0200

    Initial commit

---
+++ src/main/java/com/viseo/companion/ViseocompanionserverApplication.java
@@ -0,0 +1,12 @@
+package com.viseo.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViseocompanionserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViseocompanionserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/viseo/companion/ViseocompanionserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.viseo.companion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViseocompanionserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
