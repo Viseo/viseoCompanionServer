4513664ac64f8895a64972784905929944db2299
haifa2017 <eljarroudihaifa@gmail.com>
2017-04-25 17:28:05 +0200

    controlleur test

--- src/test/java/com/viseo/companion/ControlleurTest/UzerControlleurTest.java
+++ src/test/java/com/viseo/companion/ControlleurTest/UzerControlleurTest.java
@@ -2,16 +2,22 @@ package com.viseo.companion.ControlleurTest;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.viseo.companion.ViseocompanionserverApplication;
 import com.viseo.companion.domain.Uzer;
 import com.viseo.companion.service.UzerService;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +26,8 @@ import java.io.InputStreamReader;
 /**
  * Created by HEL3666 on 25/04/2017.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ViseocompanionserverApplication.class)
 public class UzerControlleurTest {
 
 
@@ -34,27 +42,26 @@ public class UzerControlleurTest {
         uzer.setPassword("ibllaz");
         uzer.setFirstName("ibllaz");
         uzer.setLastName("bstllaz");
+
+
+
         // Création du client et éxécution d'une requete GET
-        final org.apache.http.client.HttpClient client = HttpClientBuilder.create().build();
-        final HttpPost mockRequestPost = new HttpPost("http://localhost:8080/users");
-        ObjectMapper mapper = new ObjectMapper();
-        com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String jsonInString = ow.writeValueAsString(uzer);
-        // Ã©tablition de la requette (header+body)
-        mockRequestPost.addHeader("content-type", "application/json");
+        final HttpClient client = HttpClientBuilder.create().build();
+        final HttpPost mockRequestPost = new HttpPost("http://localhost:8080/users/");
+        final ObjectMapper mapper = new ObjectMapper();
+        final com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        final String jsonInString = ow.writeValueAsString(uzer);
+        mockRequestPost.addHeader("Content-type", "application/json");
         mockRequestPost.setEntity(new StringEntity(jsonInString));
 
-        mapper.readValue(jsonInString, Uzer.class);
-
-
-        HttpResponse mockResponse = client.execute(mockRequestPost);
+        final HttpResponse mockResponse = client.execute(mockRequestPost);
 
         // Le code retour HTTP doit être un succès (200)
         Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
-
         final BufferedReader rd = new BufferedReader(new InputStreamReader(mockResponse.getEntity().getContent()));
-        final ObjectMapper mappe = new ObjectMapper();
-        final Uzer us = mappe.readValue(rd, Uzer.class);
+        final ObjectMapper map = new ObjectMapper();
+        final Uzer us = map.readValue(rd, Uzer.class);
+
         uzerService.deletUzer(15l);
 
 
