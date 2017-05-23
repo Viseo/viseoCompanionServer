0f419ac90b81f085c531c5aa499d8283a73a3bf4
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-05-23 23:41:59 +0200

    Fix Deploiment issues

--- src/main/java/com/viseo/companion/ViseocompanionserverApplication.java
+++ src/main/java/com/viseo/companion/ViseocompanionserverApplication.java
@@ -1,14 +1,28 @@
 package com.viseo.companion;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class ViseocompanionserverApplication {
+public class ViseocompanionserverApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ViseocompanionserverApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<ViseocompanionserverApplication> applicationClass = ViseocompanionserverApplication.class;
 }
