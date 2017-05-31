732ef4ea1c27426492592e814db0f458503fceb5
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-05-31 17:32:27 +0200

    [WebSocket] Started implementing the webSocket

---
+++ src/main/java/com/viseo/companion/controller/LiveMessageHandler.java
@@ -0,0 +1,31 @@
+package com.viseo.companion.controller;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class LiveMessageHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("Session close");
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Session Open !");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        session.sendMessage(new TextMessage("Message envoyé depuis le serveur !"));
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception {
+
+        System.out.println("Message received: " + textMessage.getPayload());
+    }
+}
