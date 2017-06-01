87c028ce2355c779634c44d607ec9fd218314eec
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-06-01 11:14:41 +0200

    [WebSocket] Persistancy and broadcast

--- src/main/java/com/viseo/companion/controller/LiveMessageHandler.java
+++ src/main/java/com/viseo/companion/controller/LiveMessageHandler.java
@@ -1,11 +1,25 @@
 package com.viseo.companion.controller;
 
+import com.google.gson.Gson;
+import com.viseo.companion.domain.Comment;
+import com.viseo.companion.dto.CommentDTO;
+import com.viseo.companion.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LiveMessageHandler extends TextWebSocketHandler {
+
+    @Autowired
+    CommentService commentService;
+
+    List<WebSocketSession> activeSeesions = new ArrayList<>();
+    Gson gson = new Gson();
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
@@ -15,24 +29,35 @@ public class LiveMessageHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("Session Open !");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        activeSeesions.add(session);
         session.sendMessage(new TextMessage("Message envoyé depuis le serveur !"));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception {
         System.out.println("Message received: " + textMessage.getPayload());
-        session.sendMessage(new TextMessage(textMessage.getPayload()));
+        String savedMessage = saveMessage(textMessage.getPayload());
+        broadcastMessage(savedMessage);
     }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception)
             throws Exception {
         session.close(CloseStatus.SERVER_ERROR);
+    }
+
+    private String saveMessage(String receivedMessage) {
+        CommentDTO commentDTO = gson.fromJson(receivedMessage, CommentDTO.class);
+        CommentDTO savedComment = commentService.addComment(commentDTO);
+        return gson.toJson(savedComment);
+    }
+
+    private void broadcastMessage(String message) throws Exception {
+        if(message != null) {
+            for (WebSocketSession activeSession : activeSeesions) {
+                activeSession.sendMessage(new TextMessage(message));
+            }
+        }
     }
 
 }
